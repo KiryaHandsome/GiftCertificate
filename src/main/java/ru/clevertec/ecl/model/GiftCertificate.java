29736427bf77340aa703f7d20405f8da73fe5002
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-05-08 15:27:44 +0300

    feat: add annotations for setting time during update and save

--- src/main/java/ru/clevertec/ecl/model/GiftCertificate.java
+++ src/main/java/ru/clevertec/ecl/model/GiftCertificate.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.persistence.metamodel.StaticMetamodel;
 import lombok.AllArgsConstructor;
@@ -18,7 +20,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +35,7 @@ import java.util.List;
 @Builder
 @Entity
 @ToString
-@Table(name = "gift_certificate")
+@Table(name = "gift_certificates", schema = "gift_shop")
 @StaticMetamodel(GiftCertificate.class)
 public class GiftCertificate {
 
@@ -40,14 +45,20 @@ public class GiftCertificate {
     private String name;
     private String description;
     private Integer duration;
+
     @Column(name = "create_date")
+    @CreationTimestamp
     private ZonedDateTime createDate;
+
     @Column(name = "last_update_date")
+    @UpdateTimestamp
     private ZonedDateTime lastUpdateDate;
+
     @ManyToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
-    @JoinTable(name = "gift_certificate_tag",
+    @JoinTable(name = "gift_certificate_tag", schema = "gift_shop",
             joinColumns = @JoinColumn(name = "gift_certificate_id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id"))
+
     private List<Tag> tags = new ArrayList<>();
 
     public void addTag(Tag tag) {
